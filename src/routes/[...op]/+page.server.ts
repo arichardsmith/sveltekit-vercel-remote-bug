e4ae422c93cc6f0a1c89451fd4f4e1baa660dbdb
Richard Smith <richardmcsmith@gmail.com>
2025-12-23 08:56:41 +0900

    Shorten the ISR expiry so the clock updates quicker

--- src/routes/[...op]/+page.server.ts
+++ src/routes/[...op]/+page.server.ts
@@ -4,7 +4,7 @@ import { PREVIEW } from '$env/static/private';
 // This ISR config also applies to the remote function called at /_app/remote/[id]/[function]
 export const config: Config = {
 	isr: {
-		expiration: 60,
+		expiration: 20,
 		// Explicitly allowing the payload param makes the remote function call work correctly
 		allowQuery: PREVIEW === 'true' ? ['payload'] : undefined
 	}
