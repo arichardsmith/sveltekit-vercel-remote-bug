ee0ae8a178c4284fa68f7c6eeb78ba433dad533b
Richard Smith <richardmcsmith@gmail.com>
2025-12-22 22:20:43 +0900

    Whoops, fixed it when I meant to break it

--- src/routes/[...op]/+page.server.ts
+++ src/routes/[...op]/+page.server.ts
@@ -3,8 +3,8 @@ import type { Config } from '@sveltejs/adapter-vercel';
 // This ISR config also applies to the remote function called at /_app/remote/{id}/{function}
 export const config: Config = {
 	isr: {
-		expiration: 60,
+		expiration: 60
 		// Explicitly allowing the payload param makes the remote function call work correctly
-		allowQuery: ['payload']
+		// allowQuery: ['payload']
 	}
 };
