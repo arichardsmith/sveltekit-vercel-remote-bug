465ad809d0b33470b30bbef4bf9db8c57ba83bcf
Richard Smith <richardmcsmith@gmail.com>
2025-12-22 22:20:43 +0900

    What about subpaths?

---
+++ src/routes/ok/[...op]/+page.server.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@sveltejs/adapter-vercel';
+
+/* No ISR to mess this route up
+export const config: Config = {
+	isr: {
+		expiration: 60
+	}
+};
+*/
--- src/routes/ok2/[...op]/+page.server.ts
+++ src/routes/ok2/[...op]/+page.server.ts
@@ -1,6 +1,5 @@
 import type { Config } from '@sveltejs/adapter-vercel';
 
-// This ISR config also applies to the remote function called at /_app/remote/{id}/{function}
 export const config: Config = {
 	isr: {
 		expiration: 60,
