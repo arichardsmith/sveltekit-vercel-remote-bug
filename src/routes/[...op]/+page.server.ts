892fec5b21b440a919d80e04c4927a04d60b91cc
Richard Smith <richardmcsmith@gmail.com>
2025-12-22 21:45:17 +0900

    Test allowing the payload query param through

--- src/routes/[...op]/+page.server.ts
+++ src/routes/[...op]/+page.server.ts
@@ -3,6 +3,7 @@ import type { Config } from '@sveltejs/adapter-vercel';
 // This ISR config applies to all routes as it's in a root level catch all
 export const config: Config = {
 	isr: {
-		expiration: 60
+		expiration: 60,
+		allowQuery: ['payload']
 	}
 };
