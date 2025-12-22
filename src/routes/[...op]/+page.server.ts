52e9df66cd02a78bd570ba19e22e3df01e4901c6
Richard Smith <richardmcsmith@gmail.com>
2025-12-22 21:52:30 +0900

    Revert to the error case and tidy up

--- src/routes/[...op]/+page.server.ts
+++ src/routes/[...op]/+page.server.ts
@@ -1,9 +1,10 @@
 import type { Config } from '@sveltejs/adapter-vercel';
 
-// This ISR config applies to all routes as it's in a root level catch all
+// This ISR config applies to all routes as it's for a root level catch all page
 export const config: Config = {
 	isr: {
-		expiration: 60,
-		allowQuery: ['payload']
+		expiration: 60
+		// Explicitly allowing the payload param makes the remote function call work correctly
+		// allowQuery: ['payload']
 	}
 };
